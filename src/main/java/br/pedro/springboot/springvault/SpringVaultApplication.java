91bc86bb1023a79ec09155498618211b12d8b37e
cyborgmg <cyborgmg@gmail.com>
2020-11-29 15:27:39 -0300

    first commit

---
+++ src/main/java/br/pedro/springboot/springvault/SpringVaultApplication.java
@@ -0,0 +1,24 @@
+package br.pedro.springboot.springvault;
+
+import br.pedro.springboot.springvault.repository.FooRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVaultApplication implements CommandLineRunner {
+
+	@Autowired
+	private FooRepository fooRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVaultApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		fooRepository.findAll().forEach(foo->System.out.println(foo.getId()));
+		System.exit(0);
+	}
+}
